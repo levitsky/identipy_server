1edb5b911b30bb228577a0bd2454ed950a7fb291
Lev Levitsky <lev.levitsky@phystech.edu>
2015-04-05 18:43:02 +0300

    Rename "server" to "datasets", tweak stuff

--- identipy_server/urls.py
+++ identipy_server/urls.py
@@ -1,11 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf import settings
-from django.conf.urls.static import static
-from django.views.generic import RedirectView
 from django.contrib import admin
 
-urlpatterns = patterns('',
-	(r'^base/', include('server.urls')),
-	(r'^$', RedirectView.as_view(url='/base/base/')),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+urlpatterns = [
+	url(r'^data/', include('datasets.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+]
