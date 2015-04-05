a8431479c868b65eb5a3d9611bad91276271ee14
Lev Levitsky <lev.levitsky@phystech.edu>
2015-04-05 21:51:10 +0300

    Add file deletion

--- datasets/models.py
+++ datasets/models.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.core.files.storage import default_storage
 import os
 
 
 def my_upload_function(instance, filename):
-    allowed_extensions = set(('.raw', '.baf', '.yep', '.mgf', '.mzml', '.mzxml', '.fasta'))
+    allowed_extensions = {'.raw', '.baf', '.yep', '.mgf', '.mzml', '.mzxml', '.fasta'}
     fext = os.path.splitext(filename)[-1]
     if fext in allowed_extensions:
         upfolder = fext[1:]
     else:
-        upfolder = 'unknown'
-    return os.path.join('%s/%s' % (upfolder, filename))
+        upfolder = 'other'
+    return os.path.join('uploads', upfolder, filename)
 
 
 class Document(models.Model):
     docfile = models.FileField(upload_to=my_upload_function)
 
     def __unicode__(self):
-        return docfile
+        return unicode(docfile.name)
 
     def format(self):
-        return os.path.splitext(self.docfile)[-1]
+        return os.path.splitext(self.docfile)[-1][1:]
+
+    def delete(self):
+        super(Document, self).delete()
+        default_storage.delete(self.docfile.name)
