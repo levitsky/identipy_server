f97a1f9b50427af4500a13d2cf9c78b94ff2acb9
Lev Levitsky <lev.levitsky@phystech.edu>
2015-04-05 19:08:59 +0300

    Move template to app

--- datasets/views.py
+++ datasets/views.py
@@ -25,7 +25,7 @@ def dataset_index(request):
 
     # Render list page with the documents and the form
     return render_to_response(
-        'base/base.html',
+        'index.html',
         {'documents': documents, 'form': form},
         context_instance=RequestContext(request)
     )
