17bb4ffe98a69f16606db08d231b0e0e1cb2620e
markmipt <markmipt@localhost>
2015-04-06 18:36:43 +0300

    add authorization. add connection between uploaded docs and users

--- datasets/views.py
+++ datasets/views.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render, get_object_or_404
+from django.core.context_processors import csrf
+from django.shortcuts import render, get_object_or_404, render_to_response, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from .models import Document
 from .forms import DocumentForm
 
 def index(request):
+    c = {}
+    c.update(csrf(request))
+    c['user'] = request.user
     # Handle file upload
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc = Document(docfile = request.FILES['docfile'], userid = request.user)
             newdoc.save()
 
             # Redirect to the document list after POST
@@ -19,12 +26,13 @@ def index(request):
     else:
         form = DocumentForm() # A empty, unbound form
 
+
     # Load documents for the list page
-    documents = Document.objects.all()
+    documents = Document.objects.filter(userid=c['user'])
 
     # Render list page with the documents and the form
-    return render(request, 'datasets/index.html',
-        {'documents': documents, 'form': form})
+    c.update({'documents': documents, 'form': form})
+    return render(request, 'datasets/index.html', c)
 
 def details(request, pK):
     doc = get_object_or_404(Document, id=pK)
@@ -35,3 +43,35 @@ def delete(request, pK):
     doc = get_object_or_404(Document, id=pK)
     doc.delete()
     return HttpResponseRedirect(reverse('datasets:index'))
+
+def logout_view(request):
+    logout(request)
+    return loginview(request)
+
+def loginview(request, message=None):
+    c = {}
+    c.update(csrf(request))
+    c['message'] = message
+    return render_to_response('datasets/login.html', c)
+
+def auth_and_login(request, onsuccess='/', onfail='/login/'):
+    user = authenticate(username=request.POST['email'], password=request.POST['password'])
+    if user is not None:
+        login(request, user)
+        return redirect(onsuccess)
+    else:
+        return loginview(request, message='Wrong username or password')
+
+def user_exists(username):
+    user_count = User.objects.filter(username=username).count()
+    if user_count == 0:
+        return False
+    return True
+
+@login_required(login_url='datasets/login/')
+def secured(request):
+    c = {}
+    c.update(csrf(request))
+    c['username'] = request.user.username
+    c['userid'] = request.user.id
+    return render_to_response("index.html", c)
