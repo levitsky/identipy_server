140f9e9270655a61a4f205ab73d398e74bbc3d1e
markmipt <markmipt@localhost>
2015-04-08 13:47:21 +0300

    Add separate classes for different data types (Logic is not finished). Add choosing file page(Logic is not finished).

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Document
+from .models import SpectraFile, RawFile, FastaFile#,Document
 
-admin.site.register(Document)
+# admin.site.register(Document)
+admin.site.register(SpectraFile)
+admin.site.register(RawFile)
+admin.site.register(FastaFile)
