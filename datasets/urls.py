8f17ea8de898b7556c96ac69d7c5b543e77d5109
Mark Ivanov <markmipt@users.noreply.github.com>
2015-09-23 15:51:08 +0300

    fix for image showing in templates

--- datasets/urls.py
+++ datasets/urls.py
@@ -2,6 +2,11 @@
 from django.conf.urls import url
 from . import views
 from django.conf import settings
+
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 import os
 os.chdir(settings.BASE_DIR)
 
@@ -11,3 +16,7 @@ urlpatterns = [
     url(r'^details/([0-9]+)/', views.details, name='details'),
     url(r'^delete/([0-9]+)/', views.delete, name='delete'),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
