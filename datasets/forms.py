0d68e12a4dd051d435c57aa7e67d96b6b9ec3a35
markmipt <markmipt@localhost>
2015-12-02 19:06:15 +0300

    delete 'from email' field in contact us form

--- datasets/forms.py
+++ datasets/forms.py
@@ -204,7 +204,6 @@ class SearchParametersForm(forms.Form):
         self.fields = od
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea)
 
