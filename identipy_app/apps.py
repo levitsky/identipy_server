298012f76b7dbcb1e0d26fd77b7c145023dcc01f
Lev Levitsky <lev.levitsky@phystech.edu>
2018-11-19 23:25:26 +0300

    Create collectdead command. Also call it from wsgi.py

--- identipy_app/apps.py
+++ identipy_app/apps.py
@@ -1,25 +1,7 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-import logging
-logger = logging.getLogger(__name__)
 
 class IdentipyAppConfig(AppConfig):
     name = 'identipy_app'
-    flag = False
-
-    def ready(self):
-        if self.flag:
-            return
-        self.flag = True
-        # clean up lingrering runs
-        SearchRun = self.get_model('SearchRun')
-        try:
-            runs = SearchRun.objects.exclude(status=SearchRun.FINISHED).exclude(status=SearchRun.DEAD)
-            for r in runs:
-                r.status = SearchRun.DEAD
-                r.save()
-                logger.info('Reaping run %s from %s by %s', r.id, r.searchgroup.groupname, r.searchgroup.user.username)
-        except Exception as e:
-            logger.error('Startup cleanup failed.\n%s', e)
 
